ead61953c5bf5013c66ad2702717978e2a36ca0c
Gerasimov Vadim Vladimirovich <vagerasimov@ozon.ru>
2020-05-10 10:41:55 +0300

    add files with domashka

--- domashka1.py
+++ domashka1.py
@@ -3,4 +3,4 @@ data = int(input("Сколько вам лет? "))
 year_future = int(input("В какои году хотите узнать свой возраст? "))
 diff = year_future - year
 future_age = data + diff
-print("B ") + str(year_future) + " вам будет " + str(future_ag)
+print("B " + str(year_future) + " вам будет " + str(future_age))
