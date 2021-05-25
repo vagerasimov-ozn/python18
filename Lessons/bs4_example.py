40ab6c0fda2cacd2a8a390e6c7e17bfb3fe698b9
Vadim <wrlm@yandex.ru>
2021-05-25 17:04:07 +0300

    Добавлены базовые примеры работы парсера и bs4

---
+++ Lessons/bs4_example.py
@@ -0,0 +1,14 @@
+# import bs4
+#
+# html = """<HTML>
+#             <div class="classname">
+#                 some some
+#             </div>
+#         </HTML>
+#                 """
+# parsed_html = bs4.BeautifulSoup(html, "html.parser")
+#
+# text_found = parsed_html.find("div", attrs={"class": "classname"}).text
+#
+# print(text_found)
+
